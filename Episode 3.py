e7baacd82da0d3a443b1fe0e2f371817a9921f67
vivek-kommi <73693505+vivek-kommi@users.noreply.github.com>
2021-04-10 17:34:23 +0100

    Update Episode 3.py

--- Episode 3.py
+++ Episode 3.py
@@ -6,26 +6,19 @@ led_r = Pin(16, Pin.OUT)
 led_g = Pin(17, Pin.OUT)
 led_b = Pin(15, Pin.OUT)
 
-led_r.value(1)
-led_g.value(1)
-led_b.value(1)
 
 while True:
-  if button.value() == 1:
-    led_r.toggle()
-    utime.sleep(1)
-    led_r.toggle()
+    if button.value() == 1:
+        led_r.toggle()
+        utime.sleep(1)
+        led_r.toggle()
    
-    led_g.toggle()
-    utime.sleep(1)
-    led_g.toggle()
+        led_g.toggle()
+        utime.sleep(1)
+        led_g.toggle()
     
-    led_b.toggle()
-    utime.sleep(1)
-    led_b.toggle()
-    utime.sleep(1)
-  else:
-    led_r.value(1)
-    led_g.value(1)
-    led_b.value(1)
+        led_b.toggle()
+        utime.sleep(1)
+        led_b.toggle()
+        utime.sleep(1)
     
