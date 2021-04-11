51f0f79285b4c31d100afa437a988ba9f8724cc7
vivek-kommi <73693505+vivek-kommi@users.noreply.github.com>
2021-04-11 12:02:45 +0100

    Update Episode 4.py

--- Episode 4.py
+++ Episode 4.py
@@ -5,7 +5,7 @@ buzzer = Pin(16, Pin.OUT)
 sensor = Pin(15, Pin.IN)
 buzzer.value(0)
 while True:
-    if sensor.value()==1:
+    if sensor.value()==0:
         buzzer.value(1)
     else:
         buzzer.value(0)
