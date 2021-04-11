347d1d06ac7fe1baefdbd5adbdc96bd8be71746a
vivek-kommi <73693505+vivek-kommi@users.noreply.github.com>
2021-04-11 11:31:17 +0100

    Create Episode 4.py

---
+++ Episode 4.py
@@ -0,0 +1,11 @@
+from machine import Pin
+import utime
+
+buzzer = Pin(16, Pin.OUT)
+sensor = Pin(15, Pin.IN)
+buzzer.value(0)
+while True:
+    if sensor.value()==1:
+        buzzer.value(1)
+    else:
+        buzzer.value(0)
